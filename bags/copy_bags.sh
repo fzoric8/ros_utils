4b7e114af5cb9bf1d6f15779545b82f7735ad919
Filip Zoric <fzoric8@gmail.com>
2022-03-04 10:27:16 +0100

    Added some fpv plotting methods for joystick

--- bags/copy_bags.sh
+++ bags/copy_bags.sh
@@ -3,7 +3,7 @@
 echo "Container name: $1" 
 echo "Copy location: $2" 
 
-docker cp $1:/home/developer/moveit_ws/src/schunk_lwa4p_ros/schunk_lwa4p_control/scripts/experiment1.bag $2
+docker cp $1:/home/developer/catkin_ws/src/med_uav_control/experiments/exp_2022-01-31-17-23-43.bag $2
 
 # How to copy multiple docker files/e.g. multiple bags
 #for f in data/*txt; do docker cp $f sandbox_web_1:/usr/src/app/data/; done
